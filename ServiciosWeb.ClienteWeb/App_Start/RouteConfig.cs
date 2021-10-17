071d69d62e01e322529a82b57b5c3ab53a42292b
kerlinliberato3 <kerlinliberato3@gmail.com>
2021-10-17 12:19:03 +0800

    Casode_Uso_F5

--- ServiciosWeb.ClienteWeb/App_Start/RouteConfig.cs
+++ ServiciosWeb.ClienteWeb/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ServiciosWeb.ClienteWeb
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Casode_Uso_F5", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ ServiciosWeb.ClienteWeb/Controllers/Casode_Uso_F5Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServiciosWeb.ClienteWeb.Controllers
+{
+    public class Casode_Uso_F5Controller : Controller
+    {
+        // GET: Casode_Uso_F5
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
